e6034d627926b29515ae5edfebcd0db404e38bea
Alex-P <sdspb@yahoo.com>
2022-05-10 20:04:39 -0700

    display all fetch info, flexbox

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-const key = "";
+const key = "038e7afc2a1e68b9343e4467ba7853b1";
 const weather = {
   fetchWeather: function (city) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`)
       .then((response) => response.json())
-      .then((data) => (this.renderWeather(data), console.log(data)));
+      .then((data) => (this.renderWeather(data), console.log(data)))
   },
 
   renderWeather: function (data) {
@@ -13,12 +13,12 @@ const weather = {
     const { speed } = data.wind;
     console.log(name, icon, description, temp, humidity, speed);
     document.querySelector(".info__city").innerText = "Weather in " + name;
+    document.querySelector("#description").innerText = description.charAt(0).toUpperCase() + description.slice(1);
     document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector("#temp").innerText = temp;
-    document.querySelector("#humidity").innerText = humidity;
-    document.querySelector("#wind").innerText = speed;
-    document.querySelector("#description").innerText = description;
+    document.querySelector("#temp").innerText = temp + " " + " Fº";
+    document.querySelector("#humidity").innerText = "Humidity : " + humidity + "%";
+    document.querySelector("#wind").innerText = "Wind speed : " + speed  + " miles/hour";
   },
 };
 
-weather.fetchWeather("Seattle");
+weather.fetchWeather("Dallas");
