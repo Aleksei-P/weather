3b7d29051d0e518f62b036d8291c71b95abe497b
Alex-P <sdspb@yahoo.com>
2024-08-18 12:44:15 -0700

    background+F

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-const key = "";
+const key = "038e7afc2a1e68b9343e4467ba7853b1";
 const button = document.querySelector(".module__search-button");
 const inputCity = document.querySelector(".module__search-input");
 const weather = {
   fetchWeather: function (city) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`)
       .then((response) => response.json())
       .then((data) => this.renderWeather(data))
-      .catch((err) => {console.log(err)})
+      .catch((err) => {
+        console.log(err);
+      });
     },
 
   renderWeather: function (data) {
@@ -18,10 +20,11 @@ const weather = {
       document.querySelector(".info__city").innerText = "Weather in " + name;
       document.querySelector("#description").innerText = description.charAt(0).toUpperCase() + description.slice(1);
       document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-      document.querySelector("#temp").innerText = Math.round(temp) + " " + " Cº ";
+      document.querySelector("#temp").innerText = Math.round(temp) + " " + " Fº ";
       document.querySelector("#humidity").innerText = "Humidity: " + humidity + "%";
-      document.querySelector("#wind").innerText = "Wind speed: " + speed + " m/s";
-      document.querySelector(".page").style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+      document.querySelector("#wind").innerText = "Wind speed: " + speed + " miles/hour";
+      document.querySelector(".page").style.backgroundImage =
+        "url('https://images.unsplash.com/photo-1470290449668-02dd93d9420a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
     }
 
     else {
